97802c6064b29fdf738157f762c1113106b5bf46
Speedou <j.azran23@gmail.com>
2022-09-13 09:13:25 +0200

    login and password

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,11 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
+    #[Route('/accueil', name: 'app_main')]
     public function index(): Response
     {
         return $this->render('main/home.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
+
 }
