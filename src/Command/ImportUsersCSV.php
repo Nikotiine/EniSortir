cdb3cd03c0a36865d6dee97aeaa9b499f388637f
Speedou <j.azran23@gmail.com>
2022-09-19 15:04:46 +0200

    csv en cours

--- src/Command/ImportUsersCSV.php
+++ src/Command/ImportUsersCSV.php
@@ -2,34 +2,123 @@
 
 namespace App\Command;
 
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\File;
+use App\Entity\User;
+use App\Repository\CampusRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
-class ImportUsersCSV extends AbstractType
+class ImportUsersCSV extends Command
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    private EntityManagerInterface $entityManager;
+
+    private string $dataDirectory;
+    private SymfonyStyle $io;
+
+    private UserRepository $userRepository;
+    private CampusRepository $campusRepository;
+
+    private UserPasswordHasherInterface $userPasswordHasherInterface;
+
+    public function __construct(EntityManagerInterface      $entityManager, //$dataDirectory,
+                                UserRepository       $usersRepository, CampusRepository $campusRepository,
+                                UserPasswordHasherInterface $userPasswordHasherInterface)
     {
-        $builder
-            ->add('csv', FileType::class, [
-                'multiple' => false,
-                'mapped' => false,
-                'required' => true,
-                'constraints' => [
-                    new File([
-                        'mimeTypes' => ['text/csv', 'text/plain'],
-                    ])
-                ],
-                'label' => 'Envoyer le CSV'
-            ]);
+        parent::__construct();
+        //$this->dataDirectory = $dataDirectory;
+        $this->entityManager = $entityManager;
+        $this->userRepository = $usersRepository;
+        $this->campusRepository = $campusRepository;
+        $this->userPasswordHasherInterface = $userPasswordHasherInterface;
+
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    protected static $defaultName = 'app:utilisateurs-csv';
+    protected static $defaultDescription = 'Add a short description for your command';
+
+    protected function configure(): void
     {
-        $resolver->setDefaults([
-            // Configure your form options here
-        ]);
+        $this
+            ->setDescription('Importer des données en provenance d\'un fichier csv');
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new SymfonyStyle($input, $output);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->createUsers();
+
+        return Command::SUCCESS;
+    }
+
+    private function getDataFromFile(): array
+    {
+        $file = $this->dataDirectory() . '/public/ImportCSV/test_CSV.csv';
+        $fileExtension = pathinfo($file, PATHINFO_EXTENSION);
+
+        $normalizers = [new ObjectNormalizer()];
+
+        $encoders = [
+            new CsvEncoder(),
+        ];
+        $serializer = new Serializer($normalizers, $encoders);
+        $context = [CsvEncoder::DELIMITER_KEY => ';'];
+
+        $fileString = file_get_contents($file);
+
+        return $serializer->decode($fileString, $fileExtension, $context);
+
+    }
+
+    private function createUsers(): void
+    {
+        $this->io->section('CREATION DES UTILISATEURS A PARTIR DU FICHIER');
+        $usersCreated = 0;
+
+        foreach ($this->getDataFromFile() as $row) {
+            if (array_key_exists('username', $row) && !empty($row['username'])) {
+                $users = $this->userRepository->findOneBy([
+                    'username' => $row['username']
+                ]);
+                if ($users) {
+                    $users = new User();
+                    $users->setPseudo($row['username'])
+                        ->setEmail($row['email'])
+                        ->setCampus($this->campusRepository->findOneBy(['id' => $row['campus_id']]))
+                        ->setLastName($row['nom'])
+                        ->setFirstName($row['prenom'])
+                        ->setPhoneNumber($row['telephone'])
+                        ->setIsActive(true)
+                        ->setPassword($this->userPasswordHasherInterface->hashPassword($users, 'password'));
+
+                    $this->entityManager->persist($users);
+                    $usersCreated++;
+                } elseif ($users) {
+                    $this->io->caution("Utilisateur {$users->getUsername()} déjà en base");
+                }
+            }
+        }
+
+        $this->entityManager->flush();
+
+        if ($usersCreated > 1) {
+            $string = "{$usersCreated} utilisateurs créés en base de données.";
+        } elseif ($usersCreated === 1) {
+            $string = "Un utilisateur a été créé en base de données";
+        } else {
+            $string = "aucun utilisateur créé";
+        }
+
+        $this->io->success($string);
     }
 }
