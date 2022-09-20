562e031123797f796874fe7f3977accee71a86a6
Nikotiine <niko.tiine@gmail.com>
2022-09-20 10:37:48 +0200

    refacto autocommand

--- src/Command/AutoStatusCommands.php
+++ src/Command/AutoStatusCommands.php
@@ -27,24 +27,26 @@ class AutoStatusCommands
         $params = [Status::CREATE, Status::IN_PROGRESS, Status::OPEN, Status::CLOSE];
         $now = new \DateTimeImmutable();
         $events = $this->eventRepository->getActiveEvents($params);
+        $statusInProgress = $this->statusRepository->findOneBy([
+            'wording' => Status::IN_PROGRESS
+        ]);
+        $statusPast = $this->statusRepository->findOneBy([
+            'wording' => Status::PAST
+        ]);
         foreach ($events as $event) {
             $startTime = $event->getStartAt();
             $finishTimeEvent = $startTime->modify("+" . $event->getDuration() . 'minutes');
             if ($startTime < $now && $finishTimeEvent > $now) {
-                $event->setStatus($this->statusRepository->findOneBy([
-                    'wording' => Status::IN_PROGRESS
-                ]));
+                $event->setStatus($statusInProgress);
             }
             if ($finishTimeEvent < $now) {
-                $event->setStatus($this->statusRepository->findOneBy([
-                    'wording' => Status::PAST
-                ]));
+                $event->setStatus($statusPast);
             }
             $this->manager->persist($event);
-            $this->manager->flush();
+
         }
 
-
+        $this->manager->flush();
     }
 
     /**
@@ -56,20 +58,22 @@ class AutoStatusCommands
 
         $params = [Status::OPEN, Status::CLOSE];
         $events = $this->eventRepository->getOpenAndCloseEvents($params);
+        $statusClose =$this->statusRepository->findOneBy([
+            'wording' => Status::CLOSE
+        ]);
+        $statusOpen = $this->statusRepository->findOneBy([
+            'wording' => Status::OPEN
+        ]);
         foreach ($events as $event) {
             $maxRegistration = $event[Event::SELECTED_EVENT]->getMaxPeople();
             $totalRegistration = $event[Event::TOTAL_USER_REGISTERED];
             // Si le nombre d'inscrits est égale au nombre de places disponible, alors le status passe en CLOSE
             if ($totalRegistration == $maxRegistration && $event[Event::SELECTED_EVENT]->getStatus()->getWording()!=Status::CLOSE) {
-                $event[Event::SELECTED_EVENT]->setStatus($this->statusRepository->findOneBy([
-                    'wording' => Status::CLOSE
-                ]));
+                $event[Event::SELECTED_EVENT]->setStatus($statusClose);
             }
             // Si le nombre d'inscrits est inférieur au nombre de places disponible, alors le status passe en OPEN
             if ($totalRegistration < $maxRegistration && $event[Event::SELECTED_EVENT]->getStatus()->getWording()!=Status::OPEN ) {
-                $event[Event::SELECTED_EVENT]->setStatus($this->statusRepository->findOneBy([
-                    'wording' => Status::OPEN
-                ]));
+                $event[Event::SELECTED_EVENT]->setStatus($statusOpen);
             }
             $this->manager->persist($event[Event::SELECTED_EVENT]);
             $this->manager->flush();
--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Event;
+use App\Entity\Status;
 use App\Entity\User;
 use App\Model\EventsFilterModel;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
