0a641c448117ff42b52957cf81d5976dd7204a6e
Nikotiine <niko.tiine@gmail.com>
2022-09-22 10:08:29 +0200

    fix :push

--- assets/script/easyAdminUser.js
+++
@@ -1,6 +0,0 @@
-    const titre = document.getElementsByClassName('title');
-
-    function init() {
-        alert("coucou");
-    }
-    document.onload=init();
--- migrations/Version20220922080755.php
+++ migrations/Version20220922080755.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220921081741 extends AbstractMigration
+final class Version20220922080755 extends AbstractMigration
 {
     public function getDescription(): string
     {
